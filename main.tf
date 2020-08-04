edcbc84aab311fc8a3302c8e78f5e16addbfe111
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2020-08-04 15:23:19 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "artifactory" {
-	username = "austin.thao@armory.io"
-	password = "AKCp5fTQwha8XMv8GXez8UgJoJQeMi4R9m4QTBxMKSJaevdGeUHYkWzv8e9zAM6kmWQm9H7Us"
+	url =  "https://armory.jfrog.io/artifactory/"
+	api_key = "AKCp5fTQwha8XMv8GXez8UgJoJQeMi4R9m4QTBxMKSJaevdGeUHYkWzv8e9zAM6kmWQm9H7Us"
 }
 
 resource "artifactory_user" "foobar" {

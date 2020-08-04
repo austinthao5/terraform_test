bd756383a1698e10379305780199c467c2830a8b
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2020-08-04 15:17:07 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 provider "artifactory" {
-    api_key = "AKCp5fTQwha8XMv8GXez8UgJoJQeMi4R9m4QTBxMKSJaevdGeUHYkWzv8e9zAM6kmWQm9H7Us"
+	username = "austin.thao@armory.io"
+	password = "AKCp5fTQwha8XMv8GXez8UgJoJQeMi4R9m4QTBxMKSJaevdGeUHYkWzv8e9zAM6kmWQm9H7Us"
 }
 
 resource "artifactory_user" "foobar" {

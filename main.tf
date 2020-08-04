da05f620c29bc4bca5c6074926918b4cb0e405ef
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2020-08-04 14:18:21 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
-  acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
 resource "artifactory_user" "foobar" {
 	name  = "the.dude"
   email = "the.dude@domain.com"

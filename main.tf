719e548603e6205bfdcdd5bb3ffe283f341d7d9d
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2020-08-04 15:09:08 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
+provider "artifactory" {
+    api_key = "AKCp5fTQwha8XMv8GXez8UgJoJQeMi4R9m4QTBxMKSJaevdGeUHYkWzv8e9zAM6kmWQm9H7Us"
+}
+
 resource "artifactory_user" "foobar" {
 	name  = "the.dude"
   	email = "the.dude@domain.com"
-	api_key = "AKCp5fTQwha8XMv8GXez8UgJoJQeMi4R9m4QTBxMKSJaevdGeUHYkWzv8e9zAM6kmWQm9H7Us"
 }

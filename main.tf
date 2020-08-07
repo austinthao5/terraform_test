a4f9b752c997a10dbb4efbd73b00cb40e42846ab
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2020-08-07 11:29:41 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,35 +1,3 @@
-provider "artifactory" {
-	url =  "https://armory.jfrog.io/artifactory/"
-	api_key = "AKCp5fTQwha8XMv8GXez8UgJoJQeMi4R9m4QTBxMKSJaevdGeUHYkWzv8e9zAM6kmWQm9H7Us"
-}
+provider "hxp" {
 
-resource "artifactory_user" "foobar" {
-	name  = "NAME_HERE_TEST"
-  	email = "EMAIL@EMAIL.COM"
-}
-
-
-resource "artifactory_user" "foobar1" {
-	name  = "NAME_HERE_TEST1"
-  	email = "EMAIL1@EMAIL.COM"
-}
-
-resource "artifactory_user" "foobar2" {
-	name  = "NAME_HERE_TEST2"
-  	email = "EMAIL2@EMAIL.COM"
-}
-
-resource "artifactory_user" "foobar3" {
-	name  = "NAME_HERE_TEST3"
-  	email = "EMAIL3@EMAIL.COM"
-}
-
-resource "artifactory_user" "foobar4" {
-	name  = "NAME_HERE_TEST4"
-  	email = "EMAIL4@EMAIL.COM"
-}
-
-resource "artifactory_user" "foobar5" {
-	name  = "NAME_HERE_TEST5"
-  	email = "EMAIL5@EMAIL.COM"
 }

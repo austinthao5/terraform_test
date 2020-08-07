be02c864e6066d613bf7271cb149881c016d5bc5
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2020-08-07 11:46:44 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 provider "artifactory" {
+	url =  "https://armory.jfrog.io/artifactory/"	
+	api_key = "AKCp5fTQwha8XMv8GXez8UgJoJQeMi4R9m4QTBxMKSJaevdGeUHYkWzv8e9zAM6kmWQm9H7Us"	
+}	
 
+resource "artifactory_user" "foobar" {	
+	name  = "NAME_HERE_TEST"	
+  	email = "EMAIL@EMAIL.COM"	
 }

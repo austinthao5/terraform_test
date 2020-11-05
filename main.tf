4cb4057789c547dbd046f45e7586d649e556cf8e
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2020-11-04 17:38:12 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,9 @@
-provider "artifactory" {
-}	
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}

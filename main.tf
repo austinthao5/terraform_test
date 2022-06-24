8a269ef9d443bc9dc92ab5072c5c16b374cbea32
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2022-06-24 14:18:49 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
-  acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+resource "example_server" "my-server-name" {
+	uuid_count = "1"
 }

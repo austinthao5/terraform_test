c7d58fed0ecd47150b09d038cd5dbab303660546
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2022-06-24 20:23:00 -0700

    Update version.tf

--- version.tf
+++ version.tf
@@ -2,7 +2,7 @@ terraform {
     required_providers {
         example = {
             version = "~> 1.0.0"
-            source  = "linux_amd64/terraform-provider-example"
+            source  = "linux_amd64/customtfprovider"
         }
     }
 }

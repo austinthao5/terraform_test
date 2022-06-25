a3fa527f1b79bcabc044727aa26941695048c057
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2022-06-24 20:00:46 -0700

    Update version.tf

--- version.tf
+++ version.tf
@@ -1,3 +1,8 @@
-resource "example_server" "my-server-name" {
-	uuid_count = "1"
+terraform {
+    required_providers {
+        example = {
+            version = "~> 1.0.0"
+            source  = "terraform-example.com/exampleprovider/example"
+        }
+    }
 }

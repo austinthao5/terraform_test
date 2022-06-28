9f9e0679da3bb7fdc3965abf99b0d362fd46fa8b
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2022-06-27 20:44:40 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,10 @@
+terraform {
+  required_version = ">= 0.12.0"
 
+  backend "s3" {
+    region         = "us-west-2"
+    bucket         = "austin-artifacts"
+    key            = "terraform_backend/example.tfstate"
+    encrypt        = "true"
+  }
+}

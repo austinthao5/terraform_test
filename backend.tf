bf804c3990c5c33ed9050a1062899f43445da300
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2022-06-27 21:27:46 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12.0"
 
   backend "s3" {
-    region         = "us-west-1"
+    region         = "us-east-2"
     bucket         = "austin-artifacts"
     key            = "terraform_backend/example_backend.tfstate"
     encrypt        = true

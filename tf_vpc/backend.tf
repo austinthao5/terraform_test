1361d3e5e920dedf430f60afb7dbc05a82968713
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2022-11-17 21:43:20 -0800

    Create backend.tf

---
+++ tf_vpc/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  
+  backend "s3" {
+    region         = "us-east-2"
+    bucket         = "austin-artifacts"
+    key            = "terraform_backend/ec2-vpc.tfstate"
+    encrypt        = true
+  }
+}

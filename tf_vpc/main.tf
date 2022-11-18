329ba2545d7c0f8613b3ae9558349d567d8d5918
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2022-11-18 00:08:41 -0800

    Update main.tf

--- tf_vpc/main.tf
+++ tf_vpc/main.tf
@@ -19,6 +19,6 @@ resource "aws_subnet" "main-subnet" {
 
   tags = {
     Name = "${var.basename}-subnet-${each.key}"
-    immutable_metadata = "{" + "\"purpose\" + "\": \" + "\"ec2-vpc-1\" + "\"}\"
+    immutable_metadata = "\"10.0.0.1\"
   }
 }

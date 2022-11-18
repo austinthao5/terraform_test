d572063967067f8cfc7e08271916e1bcaf010983
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2022-11-17 23:09:00 -0800

    Update backend.tf

--- tf_vpc/backend.tf
+++ tf_vpc/backend.tf
@@ -1,9 +1,4 @@
-terraform {
-  backend "s3" {
-    region         = "us-east-2"
-    bucket         = "austin-artifacts"
-    key            = "terraform_backend/ec2-vpc.tfstate"
-    encrypt        = true
-  }
-
-}
+region         = "us-east-2"
+bucket         = "austin-artifacts"
+key            = "terraform_backend/ec2-vpc.tfstate"
+encrypt        = true

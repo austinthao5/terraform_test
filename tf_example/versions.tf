91dd1f0064739dc86cf1db8a28fccfc6146725c5
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2022-12-02 10:04:26 -0800

    Update versions.tf

--- tf_example/versions.tf
+++ tf_example/versions.tf
@@ -4,4 +4,10 @@ terraform {
       version = "~> 1.0.0"
     }
   }
+  backend "s3" {
+    region         = "us-east-2"
+    bucket         = "austin-artifacts"
+    key            = "terraform_backend/example_backend.tfstate"
+    encrypt        = true
+  }
 }
